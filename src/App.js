84f0be0df6d05b6c4f4bc80bacc61f18a26f19f5
ying.andrews <ying.andrews@gmail.com>
2023-10-04 16:01:15 -0400

    initial commit.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+
 import './App.css';
+function Header(props) {
+  return (
+    <header>
+      <h1>{props.name}'s Kitchen</h1>
+    </header>
+  );
+}
+function Main(props) {
+  return (
+    <section>
+      <img
+        height={200}
+        src="./restaurant.jpeg"
+        alt="A server presents two plates at a fancy restaurant"
+      />
+      <ul>
+        {props.dishes.map((dish) => (
+          <li key={dish.id}>
+            {dish.title}
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+function Footer(props) {
+  return (
+    <footer>
+      <p>Copyright {props.year}</p>
+    </footer>
+  );
+}
+
+const dishes = [
+  "Black Bean Soup",
+  "Macaroni and Cheese",
+  "Salmon and Potatoes",
+  "Pizza"
+];
+const dishObjects = dishes.map(
+  (dish, i) => ({
+    id: i,
+    title: dish
+  })
+);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header name="Cindy" />
+      <Main
+        adjective="amazing"
+        dishes={dishObjects}
+      />
+      <Footer
+        year={new Date().getFullYear()}
+      />
     </div>
   );
 }
