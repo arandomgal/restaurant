8e26d7c0ba8ceb47927a353bed166f72065ea319
ying.andrews <ying.andrews@gmail.com>
2023-11-15 13:33:40 -0500

    Apply 'useState' hook to toggle language settings.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-const dishesWithKeys = [
+const englishMenu = [
   {
       id: 3, // Used in JSX as a key
       name: 'Mapo Tofu'
   },
   {
       id: 2, // Used in JSX as a key
-      name: 'Chili with Cornbread'
+      name: 'Chili & Cornbread'
   },
   {
       id: 1, // Used in JSX as a key
       name: 'Beef Curry'
+  }
+];
+
+const japaneseMenu = [
+  {
+      id: 3, // Used in JSX as a key
+      name: '麻婆豆腐'
+  },
+  {
+      id: 2, // Used in JSX as a key
+      name: 'チリ＆コーンブレッド'
+  },
+  {
+      id: 1, // Used in JSX as a key
+      name: 'ビーフカレー'
   }
 ];
 
@@ -22,22 +38,46 @@ function ImageSection() {
   )
 }
 
-function Header({name}) {
-  return (
+function Header(p) {
+  if (p.menuName == "englishMenu")
+    return (
+        <header>
+            <h1>Welcome to {p.name}'s Kitchen</h1>
+        </header>
+    );
+  else if (p.menuName == "japaneseMenu")
+    return (
       <header>
-          <h1>Welcome to {name}'s Kitchen</h1>
+          <h1>いらっしゃいませ</h1>
       </header>
-  );
+    );
 }
 
 function Main(x) {
-  return (
+  if (x.menu == "englishMenu")
+    return (
       <section>
           <ul>
-              {x.dishlist.sort((a, b) => a.id - b.id).map(dish => (<li key={dish.id}>{dish.name}</li>))}
+            {englishMenu.map((dish) => (<li key={dish.id}>{dish.name}</li>) )} 
           </ul>
       </section>
-  );
+    );
+  else if (x.menu == "japaneseMenu")
+    return (
+      <section>
+          <ul>
+            {japaneseMenu.map((dish) => (<li key={dish.id}>{dish.name}</li>) )} 
+          </ul>
+      </section>
+    );
+  else
+    return (
+      <section>
+          <ul>
+            {englishMenu.map((dish) => (<li key={dish.id}>{dish.name}</li>) )} 
+          </ul>
+      </section>
+    );
 }
 
 function Footer(p) {
@@ -49,11 +89,14 @@ function Footer(p) {
 }
 
 function App() {
+  const [menuName, setLanguage] = useState("englishMenu");
   return (
     <div className="App">
-           <Header name="Michelle" />
+           <Header name="Michelle" menuName={menuName}/>
            <ImageSection />
-           <Main dishlist={dishesWithKeys} />
+           <Main menu={menuName}/>
+           <button onClick={() => setLanguage("englishMenu")}>English Menu</button>
+           <button onClick={() => setLanguage("japaneseMenu")}>Japanese Menu</button>
            <Footer
                year={new Date().getFullYear()}
            />
