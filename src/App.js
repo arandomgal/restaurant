a05f01d2f84889c82d176e2e15981f95b040248a
ying.andrews <ying.andrews@gmail.com>
2023-11-15 12:42:30 -0500

    Modify the app slightly with different variable names.

--- src/App.js
+++ src/App.js
@@ -1,63 +1,62 @@
-
+import logo from './logo.svg';
 import './App.css';
-function Header(props) {
-  return (
-    <header>
-      <h1>{props.name}'s Kitchen</h1>
-    </header>
-  );
-}
-function Main(props) {
-  return (
-    <section>
-      <img
-        height={200}
-        src="./restaurant.jpeg"
-        alt="A server presents two plates at a fancy restaurant"
-      />
-      <ul>
-        {props.dishes.map((dish) => (
-          <li key={dish.id}>
-            {dish.title}
-          </li>
-        ))}
-      </ul>
-    </section>
-  );
-}
 
-function Footer(props) {
-  return (
-    <footer>
-      <p>Copyright {props.year}</p>
-    </footer>
-  );
-}
-
-const dishes = [
-  "Black Bean Soup",
-  "Macaroni and Cheese",
-  "Salmon and Potatoes",
-  "Pizza"
+const dishesWithKeys = [
+  {
+      id: 3, // Used in JSX as a key
+      name: 'Mapo Tofu'
+  },
+  {
+      id: 2, // Used in JSX as a key
+      name: 'Chili with Cornbread'
+  },
+  {
+      id: 1, // Used in JSX as a key
+      name: 'Beef Curry'
+  }
 ];
-const dishObjects = dishes.map(
-  (dish, i) => ({
-    id: i,
-    title: dish
-  })
-);
+
+function ImageSection() {
+  return (
+      <img src="restaurant.jpeg" height={200} alt="A server presents 2 plates in a restaurant." />
+  )
+}
+
+function Header(p) {
+  return (
+      <header>
+          <h1>Welcome to {p.name}'s Kitchen</h1>
+      </header>
+  );
+}
+
+function Main(x) {
+  return (
+      <section>
+          <ul>
+              {x.dishlist.sort((a, b) => a.id - b.id).map(dish => (<li key={dish.id}>{dish.name}</li>))}
+          </ul>
+      </section>
+  );
+}
+
+function Footer(p) {
+  return (
+      <footer>
+          <p>Copyright {p.year}</p>
+      </footer>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <Header name="Cindy" />
-      <Main
-        adjective="amazing"
-        dishes={dishObjects}
-      />
-      <Footer
-        year={new Date().getFullYear()}
-      />
+           <Header name="Michelle" />
+           <ImageSection />
+           <Main dishlist={dishesWithKeys} />
+           <Footer
+               year={new Date().getFullYear()}
+           />
     </div>
   );
 }
